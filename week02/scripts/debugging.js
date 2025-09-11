b3d86432c672c98ecb484f6610d366e8bd8130ef
acottam <adam@gofishcreative.com>
2025-09-10 20:19:29 -0700

    updates

--- week02/scripts/debugging.js
+++ week02/scripts/debugging.js
@@ -1,6 +1,10 @@
 // get Elements from the document
 const radiusOutput = document.getElementById('radius');
 const areaOutput = document.querySelector('#area');
+let babygender = 'Boy';
+
+const reveal = document.getElementById('reveal');
+reveal.textContent = `🎉 Congratulations! You are having a <strong>${babygender}</strong>!`;
 
 let area = 0;
 const PI = 3.14159;
@@ -14,3 +18,5 @@ radius = 20;
 area = PI * radius * radius;
 radiusOutput.textContent = radius;
 areaOutput.textContent = area;
+
+
