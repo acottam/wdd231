37bdff9a5dcfbd60051ce191d945e5c7932fbfbd
acottam <adam@gofishcreative.com>
2025-11-06 22:39:27 -0500

    Courses Home Page - Updates

--- scripts/main.js
+++ scripts/main.js
@@ -32,12 +32,64 @@ if (toggleBtn && nav) {
     if (href === path) a.classList.add("is-active");
   });
 })();
+  
+// ---- Courses Grid ---- //
+// Use: `courses` array defined in scripts/courses.js
+(function initCourses() {
+  const grid = document.getElementById("course-grid");
+  const totalEl = document.getElementById("credit-total");
+  const filterBtns = document.querySelectorAll(".filter");
+
+  if (!grid || !totalEl || !filterBtns.length || !window.courses) return;
+
+  // Render Articles
+  const render = (subject = "ALL") => {
+    
+    // Filter List
+    const list =
+      subject === "ALL"
+        ? courses
+        : courses.filter((c) => c.subject === subject);
+
+    // Build Article Blocks
+    grid.innerHTML = list
+      .map(
+        (c) => `
+          <article class="card ${c.completed ? "course--completed" : "course--incomplete"}">
+            <div class="card__body">
+              <h3>${c.subject} ${c.number} -  ${c.title}</h3>
+              <p class="card__meta">${c.credits} credits • ${c.certificate}</p>
+              <p>${c.description}</p>
+              <p class="card__meta">Tech: ${c.technology.join(", ")}</p>
+            </div>
+          </article>`
+      )
+      .join("");
+
+    // Credit Hours
+    const credits = list.reduce(
+      (sum, c) => sum + (Number(c.credits) || 0),
+      0
+    );
+    
+    // Display Total Credits1
+    totalEl.textContent = `Total credits in view: ${credits}`;
+  };
+
+  // Default view
+  render("ALL");
+
+  // Add Clicks for filters
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      filterBtns.forEach((b) => b.classList.remove("is-active"));
+      btn.classList.add("is-active");
+      render(btn.dataset.filter);
+    });
+  });
+})();
 
 // ---- Footer ---- //
 const today = new Date();
-document.getElementById(
-  "currentyear"
-).innerHTML = `&copy;${today.getFullYear()}`;
-document.querySelector(
-  "#lastmodified"
-).textContent = `Last Modification: ${document.lastModified}`;
+document.getElementById("currentyear").innerHTML = `&copy;${today.getFullYear()}`;
+document.querySelector("#lastmodified").textContent = `Last Modification: ${document.lastModified}`;
