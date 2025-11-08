566fdc197f5c6e865cb8006ce4bf0a928024517b
acottam <adam@gofishcreative.com>
2025-11-07 23:46:41 -0500

    Chamber of Commerce - Updates

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -10,6 +10,55 @@ if (menuBtn && nav) {
   });
 }
 
+const url = 'data/members.json';
+const cards = document.querySelector('#cards');
+
+async function getMemberData() { 
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data.members);
+}
+
+getMemberData();
+
+const displayMembers = (members) => {
+  members.forEach((member) => {
+    // Create elements to add to the div.cards element
+    let card = document.createElement('section');
+    let fullName = document.createElement('h2');
+    let logo = document.createElement('img');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let website = document.createElement('a');
+
+    // Build the h2 content out to show the member's full name
+    fullName.textContent = `${member.name}`; 
+    // Build the image logo by setting all the relevant attributes
+    logo.setAttribute('src', `images/${member.image}`);
+    logo.setAttribute('alt', `Logo of ${member.name}`); 
+    logo.setAttribute('loading', 'lazy');
+    //logo.setAttribute('width', '200');
+    logo.setAttribute('height', '100');
+
+    // Build the address, phone, and website content
+    address.textContent = `Address: ${member.address}`;
+    phone.textContent = `Phone: ${member.phone}`;
+    website.textContent = member.website;
+    website.setAttribute('href', member.website);
+    website.setAttribute('target', '_blank');
+    website.setAttribute('rel', 'noopener');
+
+    // Append the section(card) with the created elements
+    card.appendChild(fullName); 
+    card.appendChild(logo);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(website);
+
+    cards.appendChild(card);
+  }); // end of arrow function and forEach loop
+} 
+
 /* ========== Footer ========== */
 // use the date object
 let today = new Date();
